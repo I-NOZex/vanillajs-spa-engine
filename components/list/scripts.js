e8ed9e1193ce83c2f061529105cf4fc86527ae49
I-NOZex <tiagofm.profissional@outlook.pt>
2021-12-22 17:25:51 +0100

    binding lists

--- components/list/scripts.js
+++ components/list/scripts.js
@@ -1,22 +1,86 @@
-export {
-    model,
-    methods
-}
+const LOG = false;
 
-const model = () => {
-    return {
-        static : {
-            title: 'title',
-            items: [
-                'item 1',
-                'item 2'
-            ]
-        },        
+class ViewModel {
+    model;
+
+    constructor(initialModel) {
+        const _self = this;
+
+        const handler = {
+            get(target, prop) {
+                //LOG && console.log({ type: 'get', target, prop });
+                return Reflect.get(target, prop);
+            },
+            set(target, prop, value) {
+                LOG && console.log({ type: 'set', target, prop, value });
+                if (prop !== '__bindings__') _self.updateUI(prop, value);
+                return Reflect.set(target, prop, value);
+            }
+        };
+
+        this.model = new Proxy(initialModel, handler);
+    }
+
+    updateUI(prop, value) {
+        let observable = this.model.__bindings__[prop];
+
+        if (!observable || !observable?.mutationAttr) {
+            if (Array.isArray(value)) {
+                const pattern = new RegExp(`^${prop}\\[(\\d+)\\]`);
+                let count = 0;
+                Object.keys(this.model.__bindings__).forEach((k) => {
+                    if (!k.match(pattern)) return;
+                    this.updateUI(`${k}`, value[count]);
+                    count++;
+                });
+            } else {
+                console.warn('Model muttate but no UI bind to update could be found');
+                return;
+            }
+        }
+
+        if (observable.mutationAttr === 'content') {
+            const newValue = `<bind>${value}</bind>`;
+            observable.target.innerHTML = observable.target.innerHTML.replace(
+                observable.replaceValue,
+                newValue
+            );
+            observable.replaceValue = newValue;
+        } else if (
+            observable.mutationAttr === 'value' &&
+            observable.replaceAttr === 'value'
+        ) {
+            observable.target.value = value;
+        }
+        LOG && console.log(observable);
     }
 }
 
-const methods = {
+export { viewModel, methods };
 
-    doSomething : () => console.log('something'),
-    hey : (e) => console.log('hey', e),
-}
+const methods = {
+    doSomething() {
+        this.title = 'helloooooo';
+    },
+    hey(e) {
+        this.initVal += 3;
+    }
+};
+
+const viewModel = new ViewModel({
+    __bindings__: {},
+
+    title: 'title 1',
+    test: 'test 2',
+    initVal: 69,
+
+    items: [{ item: 'one' }, { item: 'two' }],
+
+    list: ['one 1', 'two 2'],
+
+    static: {
+        title: 'title 2',
+
+        items: ['item 1', 'item 2']
+    }
+});
--- js/app.js
+++ js/app.js
@@ -1,111 +1,108 @@
-
 //#region
 class EventBus {
-  /**
-   * Initialize a new event bus instance.
-   */
-  constructor() {
-      this.bus = document.createElement('eventbus-proxy');
-      this.eventList = [];
-      window.$EventBus = this;
-  }
+    /**
+     * Initialize a new event bus instance.
+     */
+    constructor() {
+        this.bus = document.createElement('eventbus-proxy');
+        this.eventList = [];
+        window.$EventBus = this;
+    }
 
-  /**
-   * Add an event listener.
-   */
-  on(event, callback, options = {}) {
-      this.bus.addEventListener(event, callback, options);
-      this.eventList.push({ type: event, fn: callback.toString(), options });
-  }
+    /**
+     * Add an event listener.
+     */
+    on(event, callback, options = {}) {
+        this.bus.addEventListener(event, callback, options);
+        this.eventList.push({ type: event, fn: callback.toString(), options });
+    }
 
-  /**
-   * Remove an event listener.
-   */
-  off(event, callback) {
-      this.bus.removeEventListener(event, callback);
-      // eslint-disable-next-line max-len
-      this.eventList = this.eventList.filter(
-          (ev) =>
-              !(ev.type === event && ev.fn.toString() === callback.toString())
-      );
-  }
+    /**
+     * Remove an event listener.
+     */
+    off(event, callback) {
+        this.bus.removeEventListener(event, callback);
+        // eslint-disable-next-line max-len
+        this.eventList = this.eventList.filter(
+            (ev) =>
+                !(ev.type === event && ev.fn.toString() === callback.toString())
+        );
+    }
 
-  /**
-   * Dispatch an event.
-   */
-  emit(event, detail = {}) {
-      this.bus.dispatchEvent(new CustomEvent(event, { detail }));
-      this.eventList = this.eventList.filter(
-          (ev) => !(ev.type === event && ev.options?.once === true)
-      );
-  }
+    /**
+     * Dispatch an event.
+     */
+    emit(event, detail = {}) {
+        this.bus.dispatchEvent(new CustomEvent(event, { detail }));
+        this.eventList = this.eventList.filter(
+            (ev) => !(ev.type === event && ev.options?.once === true)
+        );
+    }
 
-  listAllEventListeners() {
-      return this.eventList.sort((a, b) => a.type.localeCompare(b.type));
-  }
+    listAllEventListeners() {
+        return this.eventList.sort((a, b) => a.type.localeCompare(b.type));
+    }
 }
 //#endregion
 
-
 //#region
+const BIND_ATTRIBUTES = '[-content],[-value],[data-bind-attrs],[-loop],[data-bind-if]';
+let obsElements = {};
+
 class ViewModel extends HTMLElement {
     shadow = null;
     wrapper = null;
     templatePath = null;
     scriptsPath = null;
+    methods = null;
+    model = null;
     eventBus = new EventBus();
 
     static get observedAttributes() {
-        return [ 'title' ];
+        return ['title'];
     }
-    
 
     constructor() {
-      // Always call super first in constructor
-      super();
+        // Always call super first in constructor
+        super();
 
-      // Create a shadow root
-      this.shadow = this.attachShadow({ mode: 'open' });
+        // Create a shadow root
+        this.shadow = this.attachShadow({ mode: 'open' });
 
-      // Create spans
-      this.wrapper = document.createElement('div');
-      this.shadow.appendChild(this.wrapper);
+        // Create spans
+        this.wrapper = document.createElement('div');
+        this.shadow.appendChild(this.wrapper);
 
-      this.templatePath = this.getAttribute('template');
-      this.scriptsPath = this.getAttribute('scripts');
+        this.templatePath = this.getAttribute('template');
+        this.scriptsPath = this.getAttribute('scripts');
     }
 
     /**
-     * Invoked each time the custom element is appended into a document-connected element. 
-     * This will happen each time the node is moved, 
-     * and may happen before the element's contents have been fully parsed. 
-     * 
+     * Invoked each time the custom element is appended into a document-connected element.
+     * This will happen each time the node is moved,
+     * and may happen before the element's contents have been fully parsed.
+     *
      * Note: connectedCallback may be called once your element is no longer connected, use Node.isConnected to make sure.
      */
     async connectedCallback() {
       const $templateDom = await this.loadTemplate(this.templatePath);
 
       const $templateNode = $templateDom.getElementsByTagName('template');
-      /*const scriptNode = $templateDom.getElementsByTagName('script');
-      const x = await import(scriptNode[0])
-      var script = document.createElement('script');
-      script.innerText = scriptNode.textContent;
-     this.wrapper.appendChild(script);*/
 
-      //console.log(scriptNode[0].innerText)
-
-
-      if($templateNode.length === 1) {
+      if ($templateNode.length === 1) {
           const $templateDomContent = $templateNode[0].content;
           this.wrapper.appendChild($templateDomContent);
-      } else if($templateNode.length > 1) {
+      } else if ($templateNode.length > 1) {
           console.warn('Multiple templates found');
       }
 
+      const {viewModel, methods} = await import(this.scriptsPath);
+      window.vm = this.model = viewModel.model;
+
+      this.methods = methods;
+
+      this.bindData(this.wrapper, this.model);
       this.bindEvents(this.wrapper);
-
-
-
 
       console.log('Custom square element added to page.');
       //updateStyle(this);
@@ -115,45 +112,47 @@ class ViewModel extends HTMLElement {
      * Invoked each time the custom element is disconnected from the document's DOM.
      */
     disconnectedCallback() {
-      console.log('Custom square element removed from page.');
+        console.log('Custom square element removed from page.');
     }
-  
+
     /**
      * Invoked each time the custom element is moved to a new document.
      */
     adoptedCallback() {
-      console.log('Custom square element moved to new page.');
+        console.log('Custom square element moved to new page.');
     }
 
     /**
-     * Invoked each time one of the custom element's attributes is added, removed, or changed. 
+     * Invoked each time one of the custom element's attributes is added, removed, or changed.
      * Which attributes to notice change for is specified in a static get observedAttributes method
      */
     attributeChangedCallback(name, oldVal, newVal) {
         if (oldVal !== newVal) {
-           console.log(`${name} changed from ${oldVal} to ${newVal}`)
+            console.log(`${name} changed from ${oldVal} to ${newVal}`);
         }
-      //updateStyle(this);
-
+        //updateStyle(this);
     }
 
-    loadTemplate = async(templatePath) => {
-        if (window.localStorage.getItem(templatePath) !== null && this.cachingEnabled) {
+    loadTemplate = async (templatePath) => {
+        if (
+            window.localStorage.getItem(templatePath) !== null &&
+            this.cachingEnabled
+        ) {
             const $auxNode = document.createElement('div');
-            $auxNode.innerHTML = window.localStorage.getItem(templatePath)
+            $auxNode.innerHTML = window.localStorage.getItem(templatePath);
             return $auxNode;
         }
-    
+
         return await fetch(location.origin + templatePath)
             // The API call was successful!
             .then((response) => response.text())
-    
+
             .then((html) => {
                 // Convert the HTML string into a document object
                 var parser = new DOMParser();
                 var doc = parser.parseFromString(html, 'text/html');
                 //window.localStorage.setItem(templatePath, doc.body.innerHTML);
-    
+
                 return doc;
             })
             .catch((err) => {
@@ -162,27 +161,206 @@ class ViewModel extends HTMLElement {
             });
     };
 
-    bindEvents = async($dom) => {
-      const $bindingContainers =  $dom.querySelectorAll('[_click]');
-      const methods = await import(this.scriptsPath);
+    // to parse Object paths
+    getObjPropByPath = (obj, path = '') => {
+        //console.log(obj[path], obj, path)
+      if (path.split('.').length <= 1 && !path.match(/^\w+\[\d+\]/g)) return obj[path];
 
-      $bindingContainers.forEach($el => {
-          const validEventAttrs = ['_click'];
-          const attrs = $el.attributes;
-
-          for(let i = attrs.length - 1; i >= 0; i--){
-            const attr = attrs[i];
-            if(!validEventAttrs.includes(attr.name)) return;
-            
-            $el.addEventListener(attr.name.substr(1), methods[attr.value]);
+      /* ⚠ DONT USE THIS IN PRODUCTION */
+      // all this code is proof of concept an may not follow the best practices at times
+      // like the use of "eval", I just want to simplify my life ...             
+      try {
+          return eval(`obj.${path}`);
+      } catch (e) {
+          if (e instanceof SyntaxError) {
+              console.error(e.message);
           }
-      });
-  }
-  
+      }
+    }
+
+    bindData = async ($template, model) => {
+        const $bindingContainers = $template.querySelectorAll(BIND_ATTRIBUTES);
+            $bindingContainers.forEach(($el) =>
+                this.mapBind($el, model)
+            );
+    };
+
+    mapBind = ($el, model, recursive = false) => {
+        const computeValue = (val) => {
+            // ⚠ CLOSE YOUR EYES! IT'S EVAL TIME AGAIN 😂
+            // this will allow as to perform extra processing to the data bind value 😎
+            if (!$el.dataset.bindFn || val === null || val === undefined)
+                return val;
+
+            return eval($el.dataset.bindFn)(val);
+        };
+
+        if ($el.dataset.bindIf) {
+            let dataBindValue;
+            const negate = $el.dataset.bindIf.startsWith('!');
+            if (negate)
+                dataBindValue = this.getObjPropByPath(
+                    model,
+                    $el.dataset.bindIf.substring(1)
+                );
+            else
+                dataBindValue = this.getObjPropByPath(
+                    model,
+                    $el.dataset.bindIf
+                );
+
+            const shouldRender = dataBindValue && !negate;
+            if (dataBindValue) {
+                if (negate) {
+                    $el.remove();
+                    return;
+                }
+            } else {
+                if (!negate) {
+                    $el.remove();
+                    return;
+                }
+            }
+        }
+
+        if ($el.hasAttribute('-content')) {
+            const modelKeyMap = /\{\{(?<modelProp>[\w\._]+)\}\}/g;
+            let bindings = {}
+
+            $el.innerHTML = $el.innerHTML.replace(modelKeyMap, (i, match) => {
+                let value = '';
+                if(recursive) {
+console.log($el.parentElement.cloneNode(true));
+
+                    const aliasModelIdx = model.findIndex(m => m != null);
+                    const aliasModel = model[aliasModelIdx];
+                    const aliasModelName = Object.keys(aliasModel)[0] ?? '';
+                    value =  this.getObjPropByPath(aliasModel[aliasModelName], match);
+                    bindings = {...bindings, ...{[`${aliasModelName}[${aliasModelIdx}].${match}`] : {target: $el, mutationAttr: 'content', replaceValue:`<bind>${value}</bind>`}}}
+                } else {
+                    value =  this.getObjPropByPath(model, match);
+                    bindings = {...bindings, ...{[match] : {target: $el, mutationAttr: 'content', replaceValue:`<bind>${value}</bind>`}}}
+                    
+                }
+
+           
+                recursive && console.log(model, match, value)
+                return `<bind>${value}</bind>`;
+            })
+            this.model.__bindings__ = {...this.model.__bindings__, ...bindings}
+
+            $el.removeAttribute('-content')
+        }
+
+
+        if ($el.hasAttribute('-value')) {
+            const prop = $el.getAttribute('-value');
+            const dataBindValue =  this.getObjPropByPath(model, prop);
+            $el.setAttribute('value', computeValue(dataBindValue));
+            this.model.__bindings__ = {
+                ...this.model.__bindings__,  
+                ...{[prop] : {target: $el, mutationAttr: 'value', replaceAttr: 'value'}}
+            }
+
+            $el.addEventListener('keyup', () => this.model[prop] = isNaN(this.model[prop]) ? $el.value.toString() : Number($el.value))
+
+            $el.removeAttribute('-value')
+        }
+        
+        if ($el.hasAttribute('-value')) {
+            const prop = $el.getAttribute('-value');
+            const dataBindValue =  this.getObjPropByPath(model, prop);
+            $el.setAttribute('value', computeValue(dataBindValue));
+            this.model.__bindings__ = {
+                ...this.model.__bindings__,  
+                ...{[prop] : {target: $el, mutationAttr: 'value', replaceAttr: 'value'}}
+            }
+
+            $el.addEventListener('keyup', () => this.model[prop] = isNaN(this.model[prop]) ? $el.value.toString() : Number($el.value))
+
+            $el.removeAttribute('-value')
+        }        
+
+        if ($el.dataset.bindAttrs) {
+            const dataBindAttrs = $el.dataset.bindAttrs
+                .replace(/\s+/g, '')
+                .split(',');
+
+            dataBindAttrs.forEach((binding) => {
+                let [attr, value] = binding.split(':');
+                let dataBindValue = this.getObjPropByPath(model, value);
+                $el.setAttribute(attr, computeValue(dataBindValue));
+            });
+
+            //delete $el.dataset.bindAttrs;
+        }
+
+        if ($el.hasAttribute('-loop')) {
+            const prop = $el.getAttribute('-loop');
+
+            let itemAux, listAux;
+            if(prop.includes('=>')) {
+                [itemAux, listAux] = prop.replace(/\s/g,'').split('=>');
+            }
+
+            let dataBindValues = this.getObjPropByPath(
+                model,
+                listAux
+            );
+            if (!dataBindValues) {
+                console.error(
+                    `Property "${
+                        listAux ?? prop
+                    }" not found in model [${Object.keys(model)}]`
+                );
+            }
+
+            console.log(dataBindValues, listAux, itemAux)
+
+            $el.innerHTML = $el.firstElementChild.outerHTML; //to reset the template for model updates
+
+            dataBindValues?.forEach((subModel, idx) => {
+                const $loopContainer = $el.firstElementChild.cloneNode(true);
+                $el.appendChild($loopContainer);
+
+                let mappedModel = [];
+                if(itemAux) 
+                    mappedModel[idx] = {[listAux]: {[itemAux]: subModel}};
+                else
+                    mappedModel = subModel;
+
+                const $childBindContainer =
+                    $loopContainer.querySelectorAll('*');
+
+                $childBindContainer.forEach(($child) => {
+                    this.mapBind($child, mappedModel, true);
+                });
+            });
+            $el.firstElementChild.remove();
+        }
+
+        $el.removeAttribute('-loop')
+    };
+
+    bindEvents = async ($dom) => {
+        const $bindingContainers = $dom.querySelectorAll('[_click]');
+
+        $bindingContainers.forEach(($el) => {
+            const validEventAttrs = ['_click'];
+            const attrs = $el.attributes;
+
+            for (let i = attrs.length - 1; i >= 0; i--) {
+                const attr = attrs[i];
+                if (!validEventAttrs.includes(attr.name)) return;
+
+                $el.addEventListener(attr.name.substr(1), this.methods[attr.value].bind(this.model));
+            }
+        });
+    };
 }
 
 function doSomething() {
-  alert(1)
+    alert(1);
 }
 
 // Define the new element
